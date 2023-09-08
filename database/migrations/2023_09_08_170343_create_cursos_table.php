2d02f0d11c67f807a5b60cbd8d3984cbd8418082
Victor Arana <victor@codersfree.com>
2023-09-08 13:05:12 -0500

    factories

--- database/migrations/2023_09_08_170343_create_cursos_table.php
+++ database/migrations/2023_09_08_170343_create_cursos_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->text('descripcion');
+            $table->text('categoria');
+
             $table->timestamps();
         });
     }
