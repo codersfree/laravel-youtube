48ee5fda1e41489b19683135fd4ab0f55328e662
Victor Arana <victor@codersfree.com>
2023-09-08 11:36:00 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::get('/cursos', [CursoController::class, 'index']);
 
-Route::get('/cursos/create', [CursoController::class, 'create']);
+Route::controller(CursoController::class)->group(function(){
+    Route::get('/cursos', 'index');
+    Route::get('/cursos/create', 'create');
+    Route::get('/cursos/{curso}', 'show');
+});
 
-Route::get('/cursos/{curso}', [CursoController::class, 'show']);
 
 /* Route::get('/cursos/{curso}/{categoria?}', function($curso, $categoria = null){
 
